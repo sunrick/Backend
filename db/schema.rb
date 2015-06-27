2a2f6962cb778a55d34e6d1fa6d38b97802d49e7
Robert Smiles Jr <smilesr120@gmail.com>
2015-06-27 19:12:36 -0400

    added description column to trip table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150627202849) do
+ActiveRecord::Schema.define(version: 20150627230839) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,8 +32,9 @@ ActiveRecord::Schema.define(version: 20150627202849) do
     t.integer  "duration"
     t.integer  "distance"
     t.string   "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.string   "description"
   end
 
   create_table "users", force: :cascade do |t|
