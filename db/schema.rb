06ccaa4fe7f2a5a29ec6244be379974e90ee5bee
Robert Smiles Jr <smilesr120@gmail.com>
2015-06-27 21:03:17 -0400

    removed duration and distance columns from trips table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150627230839) do
+ActiveRecord::Schema.define(version: 20150628005538) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,8 +29,6 @@ ActiveRecord::Schema.define(version: 20150627230839) do
 
   create_table "trips", force: :cascade do |t|
     t.string   "name"
-    t.integer  "duration"
-    t.integer  "distance"
     t.string   "user_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
